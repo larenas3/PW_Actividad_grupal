c5374ca7bd7fa834181388ddaebde77d205de2c7
larenas3 <larenas3@universidadean.edu.co>
2017-05-16 22:10:17 -0500

    Add files via upload

--- alcsetec.js
+++ alcsetec.js
@@ -2,12 +2,11 @@ function validarNombre(){
 	var nom = document.getElementById("nombre").value;
 	//Validar el campo "nombre"
 	if (nom == null || nom.length == 0 || /^\s+$/.test(nom)) {
-		$("#nombre").parent().attr("class","error");
-		$("#nombre").parent().children("span").show();
-		return false;
+    document.getElementsByClassName("msg")[0].innerHTML="Por favor escriba su nombre";
+    return false;
 	}else{
-		$("#nombre").parent().children("span").hide();
-	}
+    document.getElementsByClassName("msg")[0].innerHTML="";
+  }
 	return true;
 }
 
@@ -15,24 +14,43 @@ function validarCorreo(){
 	var mail = document.getElementById("correo").value;
 	//Validar el campo "correo"
 	if( !(/\S+@\S+\.\S+/.test(mail)) ) {
- 		$("#correo").parent().attr("class","error");
-		$("#correo").parent().children("span").show();
-		return false;
-	}else{
-		$("#correo").parent().children("span").hide();
-	}
-	return true;
+    document.getElementsByClassName("msg")[1].innerHTML="Por favor escriba su correo electrónico";
+    return false;
+  }else{
+    document.getElementsByClassName("msg")[1].innerHTML="";
+  }
+  return true;
 }
 
 function validarComentario(){
 	var coment = document.getElementById("comentarios").value;
 	//Validar el campo "comentarios"
 	if(coment == null || coment.length == 0 || /^\s+$/.test(coment)) {
- 		$("#comentarios").parent().attr("class","error");
-		$("#comentarios").parent().children("span").show();
-		return false;
-	}else{
-		$("#comentarios").parent().children("span").hide();
-	}
+    document.getElementsByClassName("msg")[2].innerHTML="Por favor escriba su comentario o sugerencia";
+    return false;
+  }else{
+    document.getElementsByClassName("msg")[2].innerHTML="";
+  }
 	return true;
+}
+
+function validacion(){
+  var texto = "Faltan los siguientes datos: ";
+  var respuesta = true;
+  if(!validarNombre()){
+    texto = texto.concat("\nNombre");
+    respuesta=false;
+  }
+  if(!validarCorreo()){
+    texto = texto.concat("\nCorreo");
+    respuesta=false;
+  }
+  if(!validarComentario()){
+    texto = texto.concat("\nComentario");
+    respuesta=false;
+  }
+  if(!respuesta){
+    alert(texto);
+  }
+  return respuesta;
 }
